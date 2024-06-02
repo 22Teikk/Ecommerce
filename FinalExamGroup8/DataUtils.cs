f9ac4e245e97b136e2ff237a4af9987ecf5d23bf
Teikk <nguyenkiet228@gmail.com>
2024-06-02 10:53:29 +0700

    Login Page

---
+++ FinalExamGroup8/DataUtils.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace FinalExamGroup8
+{
+    public class DataUtils
+    {
+        SqlConnection con;
+        public DataUtils()
+        {
+            string sqlCon = "Data Source=Teikk_Laptop\\SQLEXPRESS;Initial Catalog=nhom8db;Integrated Security=True";
+            con = new SqlConnection(sqlCon);
+        }
+
+        public bool checkAdmin(string username, string password)
+        {
+            con.Open();
+            string sql = "select * from [admin] where username=@u and password=@p";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.Parameters.AddWithValue("u", username);
+            cmd.Parameters.AddWithValue("p", password);
+            SqlDataReader rd = cmd.ExecuteReader();
+            if (rd.HasRows)
+            {
+                con.Close();
+                return true;
+            }
+            con.Close();
+            return false;
+        }
+    }
+}
---
+++ FinalExamGroup8/LoginPage.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FinalExamGroup8
+{
+    public partial class LoginPage : System.Web.UI.Page
+    {
+        DataUtils data = new DataUtils();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            string userName = yourUsername.Value;
+            string password = yourPassword.Value;
+            if (data.checkAdmin(userName, password))
+                Response.Redirect("/ProductPage.aspx");
+            else
+            {
+                Response.Redirect("/LoginPage.aspx");
+            };
+        }
+    }
+}
